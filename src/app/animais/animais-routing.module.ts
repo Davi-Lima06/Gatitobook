b84ab49a24c6ae8f2bb59bd789fa68bc26e1c5f5
Davi Lima <davips22012@gmail.com>
2021-12-30 19:23:04 -0300

    adicionando novo animal

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
@@ -9,7 +10,10 @@ const routes: Routes = [
     path: '', component: ListaAnimaisComponent,
     resolve: {
       animais: ListaAnimaisResolver
-    }
+    },
+  },
+  {
+    path: 'novo', component: NovoAnimalComponent
   },
   {
     path: ':animalId', component: DetalheAnimalComponent
